deda2c20b4051bd2f09bd9674b326782f573eca5
Jackson Reynolds <jtrey2488@gmail.com>
2025-04-04 00:40:28 -0400

    added enemy and walls

---
+++ scripts/enemy.gd
@@ -0,0 +1,12 @@
+extends Area2D
+
+@onready var timer = $timer
+
+func _on_body_entered(body: Node2D) -> void:
+	print("you died lol")
+	#for kian/ej this is the command that reloads the scene
+	#Make this code take you to a exit/restart meny
+	get_tree().reload_current_scene()
+	
+#func _on_timer_timeout():
+	#get_tree().reload_current_scene()
