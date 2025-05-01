b6dbc10a0fac6406f160ff13586873690bc3d985
eab3213 <eab3213@uncw.edu>
2025-05-01 16:07:01 -0400

    Game over scene fixed
    for love of god, please work

--- game_over.gd
+++ game_over.gd
@@ -1,19 +1,20 @@
 extends PopupPanel
 
-@onready var message_label = $Label
+@onready var message_label = $VBoxContainer/Label
+@onready var restart_button = $VBoxContainer/Restart
+@onready var quit_button = $VBoxContainer/Quit
 
-# Called when the node enters the scene tree for the first time.
 func _ready():
-	$Button.connect("pressed", Callable(self, "_on_restart_pressed"))
-	$Button2.connect("pressed", Callable(self, "_on_quit_pressed"))
+	# Connect button signals
+	restart_button.pressed.connect(_on_restart_pressed)
+	quit_button.pressed.connect(_on_quit_pressed)
 
 func _on_restart_pressed():
 	get_tree().reload_current_scene()
 
 func _on_quit_pressed():
 	get_tree().change_scene_to_file("res://scenes/main_menu.tscn")
-	
+
 func show_message(message: String):
 	message_label.text = message
-	visible = true
 	popup_centered()
