9da532a3663a0947f0a86e14fa9c8e173230eab7
KikiMiley <185963707+Dantekam@users.noreply.github.com>
2025-05-01 16:23:15 -0400

    Fixed everything
    for the last time

--- scripts/puckScript.gd
+++ scripts/puckScript.gd
@@ -1,9 +1,12 @@
 extends RigidBody2D
 
-const SliderFactor = 7
+const SliderFactor = 12
 const dragCoefficient = 0.01
 
+var inputAllowed = true
+@onready var puckHit = $"../soundFX"
 var is_editor_mode := false
+
 func _ready() -> void:
 	if not is_editor_mode:
 		collision_layer = 1
@@ -20,11 +23,23 @@ func _process(delta: float) -> void:
 func _input(event: InputEvent) -> void:
 	if is_editor_mode:
 		return
-	if event.is_action_pressed("ui_select"):
+	if event.is_action_pressed("ui_select") and (inputAllowed == true):
+		puckHit.play()
 		var mouse_vector = get_global_mouse_position()
 		var puck_direction = mouse_vector - self.global_position
+		Global.score += 1
 		var impulse = puck_direction.normalized() * (PuckVelocity.puckVelocity * SliderFactor)
 		apply_impulse(impulse)
+		inputAllowed = false
+		$"../Timer".start()
 
 func sliderChanged(value: float) -> void:
 	PuckVelocity.puckVelocity = value
+
+
+func _on_timer_timeout() -> void:
+	inputAllowed = true
+
+
+func _on_hit_timer_timeout() -> void:
+	pass # Replace with function body.
